6b0b6003ccb0941c340d5db0260fc73737af0e3f
rodney areri <arerironey@gmail.com>
2024-09-29 16:50:16 +0300

    re-reconfirguring the db to pgs

--- personalportfolio/settings.py
+++ personalportfolio/settings.py
@@ -110,29 +110,39 @@ WSGI_APPLICATION = 'personalportfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 #postgress
-
 DATABASES = {
-    'default': dj_database_url.parse(
-        (env('DATABASE_URL')),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
+    }
 }
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+
+# DATABASES = {
+#     'default': dj_database_url.parse(
+#         (env('DATABASE_URL')),
+#         conn_max_age=600,
+#         conn_health_checks=True,
+#     )
+# }
+# STORAGES = {
+#     # ...
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
