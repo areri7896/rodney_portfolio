62b5b4ae30862e3d4821b34d71f37bb3efbf356a
rodney areri <arerironey@gmail.com>
2024-09-29 17:16:57 +0300

    refactoring

--- personalportfolio/settings.py
+++ personalportfolio/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
 import environ
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
